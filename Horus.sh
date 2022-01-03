4c666c6e62cb8d5e6265eb39c36cab7e11b1e471
Dj4w4d H4mm4di <75491273+Djawad-Hammadi@users.noreply.github.com>
2022-01-04 00:45:41 +0100

    Update Horus.sh

--- Horus.sh
+++ Horus.sh
@@ -35,58 +35,79 @@ titi_1= cd tools
 scanner_banner= chmod +x scanning_banner.py && python3 scanning_banner.py
 tit_2= cd .. 
 firs= cd xline
+#Sara_Scanner
+sara=$(grep '"com.termuxhackers.id"' AndroidManifest.xml)
+if [[ $sara == "" ]];then
+printf ""
+elif [[ $sara != "" ]];then
+printf "\033[1;33mSara Ransomware \033[1;37m-->\033[0;32m FOUND\n\n"
+fi
+#Anubis_Scanner
+anubis=$(grep '"wocwvy.czyxoxmbauu.slsa"' AndroidManifest.xml)
+if [[ $anubis == "" ]];then
+printf ""
+elif [[ $anubis != "" ]];then
+printf "\033[1;33mAnubis 2.5 \033[1;37m-->\033[0;32m FOUND\n\n"
+fi
+#Xploit_Scanner
+xploit=$(grep '"com.remote.app"' AndroidManifest.xml)
+if [[ $xploit == "" ]];then
+printf ""
+elif [[ $xploit != "" ]];then
+printf "\033[1;33mXploitSPY \033[1;37m-->\033[0;32m FOUND\n\n"
+fi
 #L3mon_Scanner
 shitrat=$(grep '"com.example.dat.a8andoserverx"' AndroidManifest.xml)
 if [[ $shitrat == "" ]];then
-printf "\033[1;33m888RAT V1.1 \033[1;37m-->\033[0;31m NOT FOUND \n\n"
+printf ""
 elif [[ $shitrat != "" ]];then
 printf "\033[1;33m888RAT V1.1 \033[1;37m-->\033[0;32m FOUND\n\n"
 fi
 l3mon=$(grep '"com.etechd.l3mon"' AndroidManifest.xml)
 if [[ $l3mon == "" ]];then
-printf "\033[1;33mL3MON V1.1 \033[1;37m-->\033[0;31m NOT FOUND \n\n"
+printf ""
 elif [[ $l3mon != "" ]];then
 printf "\033[1;33mL3MON V1.1 \033[1;37m-->\033[0;32m FOUND\n\n"
 fi
 #AndroRat_Scanner
 androrat=$(grep '"my.app.client"' AndroidManifest.xml)
 if [[ $androrat == "" ]];then
-printf "\033[1;33mANDRORAT V1.0 \033[1;37m-->\033[0;31m NOT FOUND \n\n"
+printf ""
 elif [[ $androrat != "" ]];then
 printf "\033[1;33mANDRORAT V1.0 \033[1;37m-->\033[0;32m FOUND\n\n"
 fi
 #Metasploit_Scanner
 metasploit=$(grep '"MainBroadcastReceiver"' AndroidManifest.xml) 
 if [[ $metasploit == "" ]];then
-printf "\033[1;33mMETASPLOIT V5.0 \033[1;37m-->\033[0;31m NOT FOUND \n\n"
+printf ""
 elif [[ $metasploit != "" ]];then
-printf "\033[1;33mMETASPLOIT V5.0 \033[1;37m-->\033[0;32m FOUND\n\n"
+printf "\033[1;33mMETASPLOIT V6.0 \033[1;37m-->\033[0;32m FOUND\n\n"
 fi
 #droidjack_scanner
 droid=$(grep '"net.droidjack.server"' AndroidManifest.xml)
 if [[ $droid == "" ]];then
-printf "\033[1;33mDROIDJACK V4.4 \033[1;37m-->\033[0;31m NOT FOUND \n\n"
+printf ""
 elif [[ $droid != "" ]];then
 printf "\033[1;33mDROIDJACK V4.4 \033[1;37m-->\033[0;32m FOUND \n\n"
 fi
 #ahm_scanner
 ahm=$(grep '"ahmyth.mine.king.ahmyth.MainActivity"' AndroidManifest.xml)
 if [[ $ahm == "" ]];then
-printf "\033[1;33mAHMYTH V1.0 \033[1;37m-->\033[0;31m NOT FOUND \n\n"
+printf ""
 elif [[ $ahm != "" ]];then
 printf "\033[1;33mAHMYTH V1.0 \033[1;37m-->\033[0;32m FOUND \n\n"
 fi
 #SpynoteV.50
 spynote5=$(grep '"yps.eton.application"' AndroidManifest.xml)
 if [[ $spynote5 == "" ]];then
-printf "\033[1;33mSPYNOTE V5.0 \033[1;37m-->\033[0;31m NOT FOUND \n\n"
+printf ""
 elif [[ $spynote5 != "" ]];then
 printf "\033[1;33mSPYNOTE V5.0 \033[1;37m-->\033[0;32m FOUND \n\n"
 fi
 #spymax2_scanner
 spy2=$(grep '"package.name.suffix"' AndroidManifest.xml)
 if [[ $spy2 == "" ]];then
-printf "\033[1;33mSPYMAX V2.0 \033[1;37m-->\033[0;31m NOT FOUND \n\n"
+printf ""
 elif [[ $spy2 != "" ]];then
 printf "\033[1;33mSPYMAX V2.0 \033[1;37m-->\033[0;32m FOUND\n\n"
 fi
@@ -94,21 +115,21 @@ fi
 open= cd res/values/
 spymax=$(grep '"a"' strings.xml)
 if [[ $spymax == "" ]];then
-printf "\033[1;33mSPYMAX V1.0 \033[1;37m-->\033[0;31m NOT FOUND \n\n"
+printf ""
 elif [[ $spymax != "" ]];then
 printf "\\033[1;33mSPYMAX V1.0 \033[1;37m-->\033[0;32m FOUND \n\n"
 fi
 #spynote_scanner
 spynote=$(grep '"MER"' strings.xml)
 if [[ $spynote == "" ]];then
-printf "\033[1;33mSPYNOTE V6.4 \033[1;37m-->\033[0;31m NOT FOUND \n\n"
+printf ""
 elif [[ $spynote != "" ]];then
 printf "\033[1;33mSPYNOTE V6.4 \033[1;37m-->\033[0;32m FOUND \n\n"
 fi
 #mobihok_scanner
 mobihok=$(grep '"abcd00"' strings.xml)
 if [[ $mobihok == "" ]];then
-printf "\033[1;33mMOBIHOK V6.0 \033[1;37m-->\033[0;31m NOT FOUND \n\n"
+printf ""
 elif [[ $mobihok != "" ]];then
 printf "\033[1;33mMOBIHOK V6.0\033[1;37m-->\033[0;32m FOUND \n\n"
 fi
@@ -125,12 +146,21 @@ ask() {
 cd ../../..
 psyco_1= cd tools
 info_banner= chmod +x info_banner.py && python3 info_banner.py #Grep_Info_Banner
-if [[ $spy2 != "" ]];then
+if [[ $sara != "" ]];then
+chmod +x sara.sh
+bash sara.sh
+elif [[ $anubis != "" ]];then
+chmod +x anubis.sh
+bash anubis.sh
+elif [[ $xploit != "" ]];then
+chmod +x xploit.sh
+bash xploit.sh #Xploit_Spy_Grep_Info
+elif [[ $spy2 != "" ]];then
 chmod +x spy2.sh
-bash spy2.sh
+bash spy2.sh #spynote_2_Grep_Info
 elif [[ $shitrat != "" ]];then
 chmod +x 888.sh
-bash 888.sh
+bash 888.sh #888_Rat_Grep_Info
 elif [[ $l3mon != "" ]];then
 chmod +x l3mon.sh
 bash l3mon.sh #L3mon_Grep_Info
@@ -148,95 +178,12 @@ chmod +x ahmy.sh
 bash ahmy.sh #AHMYTH_Grep_Info
 elif [[ $androrat != "" ]];then
 chmod +x andro.sh	
-bash andro.sh
+bash andro.sh #Andro_Rat_Grep_Info
 else
 #If_Scanner_Don't_Detect_Anything
 printf "\033[1;37mWe Are Sorry But \033[0;31mNothin Detected \033[1;37mTry : \033[1;33mManual Scan033[1;37m:(\n"
 exit 0 
 fi
-}
-#Grep_Ip_Info_Function
-infocode () {
-if [[ -e xline.log ]]; then
-rm -rf xline.log
-fi
-printf "\033[0;32m┌─[\033[0;31mHorus\033[0;32m]──[\033[0;31m~\033[0;32m]─[\033[0;32mEye\033[0;32m]:\n" 
-printf "\033[1;37m└───► Enter Ip Ex : "
-read ip
-info_banner= chmod +x ip_banner.py && python3 ip_banner.py #Grep_Ip_Info_Banner
-sleep 2
-printf "\n" 
-iptracker=$(curl -s -L "www.ip-tracker.org/locator/ip-lookup.php?ip=$ip" --user-agent "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.63 Safari/537.31" > xline.log)
-continent=$(grep -o 'Continent.*' xline.log | head -n1 | cut -d ">" -f3 | cut -d "<" -f1)
-if [[ $continent != "" ]]; then
-printf "\033[1;33mContinent : \033[0;32m FOUND : \033[1;37m ($continent)\n\n" 
-elif [[ $continent == "" ]]; then
-printf "\033[1;33mContinent : \033[0;31m NOT FOUND \n\n"
-fi
-hostnameip=$(grep  -o "</td></tr><tr><th>Hostname:.*" xline.log | cut -d "<" -f7 | cut -d ">" -f2)
-if [[ $hostnameip != "" ]]; then
-printf "\033[1;33mHostname :\033[0;32m FOUND : \033[1;37m ($hostnameip)\n\n" 
-elif [[ $hostnameip == "" ]]; then 
-printf "\033[1;33mHostname :\033[0;31m NOT FOUND\n\n"
-fi 
-reverse_dns=$(grep -a "</td></tr><tr><th>Hostname:.*" xline.log | cut -d "<" -f1)
-if [[ $reverse_dns != "" ]]; then
-printf "\033[1;33mReverse_Dns : \033[0;32m FOUND : \033[1;37m ($reverse_dns)\n\n" 
-elif [[ $reverse_dns == "" ]]; then 
-printf "\033[1;33mRerverse_Dns : \033[0;31m NOT FOUND\n\n"
-fi 
-country=$(grep -o 'Country:.*' xline.log | cut -d ">" -f3 | cut -d "&" -f1)
-if [[ $country != "" ]]; then
-printf "\033[1;33mCountry : \033[0;32m FOUND : \033[1;37m ($country)\n\n" 
-elif [[ $country == "" ]] ; then 
-printf "\033[1;33mCountry : \033[0;31m NOT FOUND\n\n"
-fi 
-state=$(grep -o "tracking lessimpt.*" xline.log | cut -d "<" -f1 | cut -d ">" -f2)
-if [[ $state != "" ]]; then
-printf "\033[1;33mState : \033[0;32m FOUND : \033[1;37m ($state)\n\n" 
-elif [[ $state == "" ]];then
-printf "\033[1;33mState : \033[0;31m NOT FOUND\n\n"
-fi 
-city=$(grep -o "City Location:.*" xline.log | cut -d "<" -f3 | cut -d ">" -f2)
-if [[ $city != "" ]]; then
-printf "\033[1;33mCity : \033[0;32m FOUND : \033[1;37m ($city)\n\n" 
-elif [[ $city == "" ]]; then
-printf "\033[1;33mCity : \033[0;31m NOT FOUND\n\n"
-fi 
-isp=$(grep -o "ISP:.*" xline.log | cut -d "<" -f3 | cut -d ">" -f2)
-if [[ $isp != "" ]]; then
-printf "\033[1;33mISP : \033[0;32m FOUND : \033[1;37m ($isp)\n\n" 
-elif [[ $isp == "" ]]; then 
-printf "\033[1;33mISP : \033[0;31m NOT FOUND\n\n"
-fi 
-as_number=$(grep -o "AS Number:.*" xline.log | cut -d "<" -f3 | cut -d ">" -f2)
-if [[ $as_number != "" ]]; then
-printf "\033[1;33mAs_Number : \033[0;32m FOUND : \033[1;37m ($as_number)\n\n" 
-elif [[ $as_number == "" ]];then
-printf "\033[1;33mAs Number : \033[0;31m NOT FOUND\n\n"
-fi 
-ip_speed=$(grep -o "IP Address Speed:.*" xline.log | cut -d "<" -f3 | cut -d ">" -f2)
-if [[ $ip_speed != "" ]]; then
-printf "\033[1;33mIp_Speed : \033[0;32m FOUND : \033[1;37m ($ip_speed)\n\n" 
-elif [[ $ip_speed == "" ]] ; then 
-printf "\033[1;33mIp_Speed : \033[0;31m NOT FOUND\n\n"
-fi
-ip_currency=$(grep -o "IP Currency:.*" xline.log | cut -d "<" -f3 | cut -d ">" -f2)
-if [[ $ip_currency != "" ]]; then
-printf "\033[1;33mIp_Currency : \033[0;32m FOUND: \033[1;37m ($ip_currency)\n\n" 
-elif [[ $ip_currency == "" ]]; then 
-printf "\033[1;33mIp_Currency : \033[0;31m NOT FOUND\n\n"
-fi 
-#End_Grep_Ip_Info_Code
-}
-ipinfo() {
-#Choose Host Or Ip
-cd ..
-ok_55= cd tools
-revesehost= chmod +x reverse.py && python3 reverse.py
-printf "\n"
-infocode
-sleep 5  
 }
 repet() {
 printf "\033[0;32m┌─[\033[0;31mHorus\033[0;32m]──[\033[0;31m~\033[0;32m]─[\033[0;32mEye\033[0;32m]:\n" 
@@ -276,7 +223,6 @@ banner
 debug
 scanner
 ask
-ipinfo
 remove
 repet
 #End_Of_Script
