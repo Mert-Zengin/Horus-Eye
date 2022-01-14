387a758173795f20678dcab8d501b1d7c7f2ef1f
Dj4w3d H4mm4di <75491273+Djawad-Hammadi@users.noreply.github.com>
2022-01-14 21:46:33 +0100

    Update version.py

--- tools/version.py
+++ tools/version.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 import requests
 def version():
-	r = requests.get("https://raw.githubusercontent.com/Djawed-Hammadi/Horus-Eye/main/tools/version.txt")
+	r = requests.get("https://pastebin.com/raw/EgU4uQeT")
 	response = r.text
 	if response == "2.3":
     		print("\n\033[0;32m[ ✔ ] Latest Version .............[ \033[1;33mFound \033[0;32m]\n\n")
 	else:
-    		print("\n\033[0;32m[#]\033[1;37m New Update Detcted Please \033[0;31mUpdate This Tool\n")
+    		print("\n\033[0;32m[#]\033[1;37m New Update Detcted Please \033[0;31mUpdate This Tool\n\n")
 version()
