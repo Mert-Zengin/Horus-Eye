a57b3a52dbc8a3686dd479b0c3b2fd353c064a9d
Dj4w3d H4mm4di <75491273+Djawad-Hammadi@users.noreply.github.com>
2022-02-11 19:32:03 +0100

    Update banner.py

--- tools/banner.py
+++ tools/banner.py
@@ -16,7 +16,7 @@ print("""
 \033[1;33m                          [   ||   ]                       
 \033[1;33m                           \__||__/                          
 \033[1;33m                              --                                                        
-\033[0;34m        [---] Coded By : \033[0;31mDjawed Hammadi \033[0;34m(\033[1;33mx-line\033[0;34m) [---]
+\033[0;34m        [---] Coded By : \033[0;31mDjawad Hammadi \033[0;34m(\033[1;33mx-line\033[0;34m) [---]
 \033[0;34m[---] Horus-Eye : \033[0;31mReverse engineering tool \033[0;34m(\033[1;33mAndroid Payloads\033[0;34m) [---]
 \033[0;34m                          Version : \033[0;31m2.2
 \033[0;34m            [---] Follow me on Twitter: \033[0;35m@xlinex23 \033[0;34m[---]                             
