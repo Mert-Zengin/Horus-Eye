2773160354e214b992ac3f9439d81a17d752cfa7
Dj4w3d H4mm4di <75491273+Djawad-Hammadi@users.noreply.github.com>
2022-02-12 20:57:04 +0100

    Update version.py

--- tools/version.py
+++ tools/version.py
@@ -4,7 +4,7 @@ def version():
 	r = requests.get("https://pastebin.com/raw/EgU4uQeT")
 	response = r.text
 	if response == "2.3":
-    		print("\n\033[0;32m[ ✔ ] Latest Version .............[ \033[1;33mFound \033[0;32m]\n\n")
+    		print("\n\033[0;32m[ ✔ ] Latest Version .............[ \033[1;33mFound \033[0;32m]\n\")
 	else:
     		print("\n\033[0;32m[#]\033[1;37m New Update Detcted Please \033[0;31mUpdate This Tool\n\n")
 version()
